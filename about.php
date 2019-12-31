9d5b130c0a9f9a5c357748a8ef10fa459060fa48
tarikfayzullah <tarikfayzullah@gmail.com>
2020-01-01 00:03:20 +0600

    first commit

---
+++ about.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>About</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Pets Care Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Custom Theme files -->
+	<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+	<link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- font-awesome icons -->
+	<!-- //Custom Theme files -->
+	<!-- js -->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- //js -->
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Limelight" rel="stylesheet">
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
+	    rel='stylesheet' type='text/css'>
+	<!-- //web-fonts -->
+</head>
+
+<body>
+	<!-- banner -->
+	<div class="agileits-banner about-w3banner">
+		<div class="bnr-agileinfo">
+			<div class="banner-top w3layouts">
+				<div class="container">
+					<ul class="agile_top_section">
+						<li>
+							
+						</li>
+						<li>
+							<p><span class="glyphicon glyphicon-earphone"></span> +88 01681 186911 </p>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="banner-w3text w3layouts">
+				<h2>Animal Care Zone</h2>
+			</div>
+			
+			<!-- navigation -->
+			<div class="top-nav w3-agiletop">
+				<div class="agile_inner_nav_w3ls">
+					<div class="navbar-header w3llogo">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+
+						</button>
+						<h1><a href="index.php">Pets Care</a></h1>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<div class="w3menu navbar-left">
+							<ul class="nav navbar">
+								<li><a href="index.php" class="active">Home</a></li>
+								<li><a href="Medicare.php">Medicare</a></li>
+								<li><a href="Catagories.php">Catagories</a></li>
+								<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span data-letters="Pages">Products</span><span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="foods.php">Foods</a></li>
+										<li><a href="accessories.php">Accessories</a></li>
+									</ul>
+								</li>
+								<li><a href="about.php">About</a></li>
+							</ul>
+						</div>
+						<div class="w3ls-bnr-icons social-icon navbar-right">
+							<a href="https://twitter.com/InfoanimalInfo"><i class="fa fa-twitter"></i></a>
+							<a href="https://www.facebook.com/Animal-Information-zone-121638088549277/" class="social-button facebook"><i class="fa fa-facebook"></i></a>
+							<a href="https://plus.google.com/u/0/" class="social-button google"><i class="fa fa-google-plus"></i></a>
+							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+			<!-- //navigation -->
+		</div>
+	</div>
+	<!-- //banner -->
+	
+
+	<!-- contact -->
+		<!-- welcome -->
+	<div class="welcome agileits">
+		<div class="container">
+			<h3 class="agileits-title">Developers</h3>
+			
+			<div class="welcome-w3lsrow2">
+			<div class="row">	
+				<div class="col-md-4 col-sm-12 team-grid">
+					<div class="flip-container tu-of-hidden">
+						<div class="flipper">
+							<div class="front front-width">
+								<img src="images/img2.jpg" class="img-responsive" alt="" />
+							</div>
+							<div class="back">
+								<h4>Adison Gomes</h4>
+								<p>Pet Lover</p>
+								<div class="w3l-social">
+									<ul>
+										<li><a href="https://www.facebook.com/adison.gomes.5"><i class="fa fa-facebook"></i></a></li>
+										<li><a href="https://twitter.com/AdisonGomes1"><i class="fa fa-twitter"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-12 team-grid">
+					<div class="flip-container tu-of-hidden">
+						<div class="flipper">
+							<div class="front front-width">
+								<img src="images/img3.jpg" alt="" />
+							</div>
+							<div class="back">
+								<h4>Mehnaz Tabassum</h4>
+								<p>Pet Lover</p>
+								<div class="w3l-social">
+									<ul>
+										<li><a href="https://www.facebook.com/mehnaz.moutoshi"><i class="fa fa-facebook"></i></a></li>
+										<li><a href="https://twitter.com/mehnazmoutushee"><i class="fa fa-twitter"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-12 team-grid">
+					<div class="flip-container tu-of-hidden">
+						<div class="flipper">
+							<div class="front front-width">
+								<img src="images/img4.jpg" alt="" />
+							</div>
+							<div class="back">
+								<h4>jannatul ferdous</h4>
+								<p>Pet Lover</p>
+								<div class="w3l-social">
+									<ul>
+										<li><a href="https://www.facebook.com/jannat.rahman.315"><i class="fa fa-facebook"></i></a></li>
+										<li><a href="https://twitter.com/jannat_CSE"><i class="fa fa-twitter"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+	<!-- //contact -->
+
+	<!-- copy rights start here -->
+	<div class="copy-w3right">
+		<div class="container">
+			<div class="top-nav bottom-w3lnav">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="medicare.php">Medicare</a></li>
+					<li><a href="catagories.php">Catagories</a></li>
+					<li><a href="foods.php">Foods</a></li>
+					<li><a href="accessories.php">Accessories</a></li>
+					<li><a href="about.php">About</a></li>
+				</ul>
+			</div>
+			<p>© 2017 Animal Information Zone : All Rights Reserved | Design by : Wall Breaker</a> </p>
+		</div>
+	</div>
+	<!-- //copy right end here -->
+
+</body>
+
+</html>
---
+++ bird.php
@@ -0,0 +1,380 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Bird</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Pets Care Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Custom Theme files -->
+	<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+	<link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- font-awesome icons -->
+	<!-- //Custom Theme files -->
+	<!-- js -->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- //js -->
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Limelight" rel="stylesheet">
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
+	    rel='stylesheet' type='text/css'>
+	<!-- //web-fonts -->
+
+<script type="text/javascript">
+
+
+	function getCalForDove(monthAge) {
+		if ( monthAge = monthAge.trim() ) {
+                  if (monthAge>0 && monthAge<=2) {
+                        return "300 gm";	
+                  } else if (monthAge>2 && monthAge<=4) {
+                        return "400 gm";
+                  } else if (monthAge>4 && monthAge<=6) {
+                        return "600 gm";
+                  } else if (monthAge>6 && monthAge<=8) {
+                        return "600 gm";
+                  } else if (monthAge>8 && monthAge<=10) {
+                        return "600 gm";
+                  } else if (monthAge>10 && monthAge<=12) {
+                        return "550 gm";
+                  } else if (monthAge>12 && monthAge<=14) {
+                        return "800 gm";
+                  } else if (monthAge>14 && monthAge<=16) {
+                        return "1000 gm";
+                  } else if (monthAge>16 && monthAge<=18) {
+                        return "750 gm";
+                  } else if (monthAge>18 && monthAge<=21) {
+                        return "750 gm";
+                  } else if (monthAge>21 && monthAge<=24) {
+                        return "1000 gm";
+                  } else if (monthAge>24 && monthAge<=26) {
+                        return "800 gm";
+                  } else if (monthAge>26 && monthAge<=28) {
+                        return "1200 gm";
+                  } else if (monthAge>28 && monthAge<=30) {
+                        return "800 gm";
+                  } else if (monthAge>30 && monthAge<=32) {
+                        return "600 gm";
+                  } else if (monthAge>32 && monthAge<=35) {
+                        return "800 gm";
+                  } else if (monthAge>35 && monthAge<=36) {
+                        return "1200 gm";
+                  } else if (monthAge>36 && monthAge<=38) {
+                        return "700 gm";
+                  } else if (monthAge>38 && monthAge<=40) {
+                        return "800 gm";
+                  } else if (monthAge>40 && monthAge<=42) {
+                        return "800 gm";
+                  } else if (monthAge>42 && monthAge<=44) {
+                        return "1250 gm";
+                  } else if (monthAge>44 && monthAge<=46) {
+                        return "1250 gm";
+                  } else if (monthAge>46 && monthAge<=50) {
+                        return "800 gm";
+                  } else if (monthAge>50 && monthAge<=51) {
+                        return "800 gm";
+                  } else if (monthAge>52) {
+                        return "Cross The age Limit";
+                  } else {
+                        return "Wrong Input!";
+                  }
+            } else {
+                  return "Age field is empty!";
+            }
+	}
+
+      getNutrition(birdName) {
+      	console.log(this);
+            if ( birdName = birdName.trim() ) {
+                  if ( birdName == 'dove' ) {
+                        getCalForDove();
+                  }
+            } else {
+                  throw new Error('No bird name passed!');
+            }
+      }
+
+
+</script>
+
+
+</head>
+
+<body>
+	<!-- banner -->
+	<div class="agileits-banner about-w3banner">
+		<div class="bnr-agileinfo">
+			<div class="banner-top w3layouts">
+				<div class="container">
+					<ul class="agile_top_section">
+						<li>
+						</li>
+						<li>
+							<p><span class="glyphicon glyphicon-earphone"></span> +88 01681 186991 </p>
+						</li>
+						<li><a class="sign" href="#" data-toggle="modal" data-target="#myModal2"><i class="fa fa-sign-in" aria-hidden="true"></i> Sign In</a>							</li>
+					</ul>
+				</div>
+			</div>
+			<div class="banner-w3text w3layouts">
+				<h2>Animal Care Zone</h2>
+			</div>
+			<!-- navigation -->
+			<div class="top-nav w3-agiletop">
+				<div class="agile_inner_nav_w3ls">
+					<div class="navbar-header w3llogo">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<h1><a href="index.php">Pets Care</a></h1>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<div class="w3menu navbar-left">
+							<ul class="nav navbar">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="medicare.php">Medicare</a></li>
+								<li><a href="catagories.php">Catagories</a></li>
+								<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span data-letters="Pages">Products</span><span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="foods.php" class="active">Foods</a></li>
+										<li><a href="accessories.php">Accessories</a></li>
+									</ul>
+								</li>
+								<li><a href="about.php" >About</a></li>
+							</ul>
+						</div>
+						<div class="w3ls-bnr-icons social-icon navbar-right">
+							<a href="https://twitter.com/InfoanimalInfo" class="social-button twitter"><i class="fa fa-twitter"></i></a>
+							<a href="https://www.facebook.com/Animal-Information-zone-121638088549277/" class="social-button facebook"><i class="fa fa-facebook"></i></a>
+							<a href="https://plus.google.com/u/0/" class="social-button google" class="social-button google"><i class="fa fa-google-plus"></i></a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+			<!-- //navigation -->
+		</div>
+	</div>
+	<!-- //banner -->
+
+
+
+
+
+	<!-- Modal1 -->
+	<div class="modal fade" id="myModal2" tabindex="-1" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+
+					<div class="signin-form profile">
+						<h3 class="agileinfo_sign">Sign In</h3>
+						<div class="login-form">
+							<form action="#" method="post">
+								<input type="email" name="email" placeholder="E-mail" required="">
+								<input type="password" name="password" placeholder="Password" required="">
+								<div class="tp">
+									<input type="submit" value="Sign In">
+								</div>
+							</form>
+						</div>
+						<div class="login-social-grids">
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							</ul>
+						</div>
+						<p><a href="#" data-toggle="modal" data-target="#myModal3"> Don't have an account?</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Modal1 -->
+
+	<!-- Modal2 -->
+	<div class="modal fade" id="myModal3" tabindex="-1" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+
+					<div class="signin-form profile">
+						<h3 class="agileinfo_sign">Sign Up</h3>
+						<div class="login-form">
+							<form action="#" method="post">
+								<input type="text" name="name" placeholder="Username" required="">
+								<input type="email" name="email" placeholder="Email" required="">
+								<input type="password" class="password" name="Password" id="password1" placeholder="Password" required="" />
+								<input type="password" class="password" name="Confirm Password" id="password2" placeholder="Confirm Password" required=""
+								/>
+								<input type="submit" value="Sign Up">
+							</form>
+						</div>
+						<p><a href="#"> By clicking register, I agree to your terms</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Modal2 -->
+
+<!-- information start-->
+
+<div class="jumbotron text-center">
+  <h1>SEE YOUR BIRDS INFORMATION</h1>
+  <p>Please input your Birds Age to see Nutrition</p> 
+</div>
+
+<div class="container">
+  
+  <div class="row">
+  
+    <div class="col-sm-4"> 
+      			<div align="center" style="border: 1px solid white" >       			
+      				<h3>Austrilan dove</h3>
+      					<img src="images/austrilandove.jpg" class="img-responsive" alt="Austrilan Dove" />
+      						<p>Please Enter Your Birds Age (Month only) </p>
+      							<form name="formcalc">
+	      							<input type="text" name="txtnum1"> </br></br>
+	      							<input type="button" value="See Nutrition" onclick="getNutrition('dove')"></br></br>
+	        						<input type="text" name="textres"></br></br>
+      							</form>
+
+      							<p> If you want to know more information. please download these link : </p>
+      							<a href="resource/diamond_dove.pdf" download>Diamond Dove</a>
+      			</div>
+	</div>
+
+
+
+<div class="col-sm-4"> 
+      			<div align="center" style="border: 1px solid white" >       			
+      				<h3>Austrilan dove</h3>
+      					<img src="images/austrilandove.jpg" class="img-responsive" alt="Austrilan Dove" />
+      						<p>Please Enter Your Birds Age (Month only) </p>
+      							<form name="formbjr">
+      							<input type="text" name="txtnum1"> </br></br>
+      							<input type="button" value="See Nutrition" onclick="bjrigar()"></br></br>
+        						<input type="text" name="textres"></br></br>
+      							</form>
+
+      							<p> If you want to know more information. please download these link : </p>
+      							<a href="resource/diamond_dove.pdf" download>Diamond Dove</a>
+      			</div>
+	</div>
+
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- information end-->
+
+
+
+
+
+	<!-- copy rights start here -->
+	<div class="copy-w3right">
+			<p>© 2017 Animal Information Zone : All Rights Reserved | Design by : Wall Breaker</p>
+		</div>
+	</div>
+	<!-- //copy right end here -->
+
+
+	<script src="js/SmoothScroll.min.js"></script>
+	<!-- password-script -->
+	<script type="text/javascript">
+		window.onload = function () {
+			document.getElementById("password1").onchange = validatePassword;
+			document.getElementById("password2").onchange = validatePassword;
+		}
+
+		function validatePassword() {
+			var pass2 = document.getElementById("password2").value;
+			var pass1 = document.getElementById("password1").value;
+			if (pass1 != pass2)
+				document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+			else
+				document.getElementById("password2").setCustomValidity('');
+			//empty string means no validation error
+		}
+	</script>
+	<!-- //password-script -->
+
+
+
+	<!-- start-smooth-scrolling -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
+		});
+	</script>
+	<!-- //end-smooth-scrolling -->
+	<!-- smooth-scrolling-of-move-up -->
+	<script type="text/javascript">
+		$(document).ready(function () {
+			/*
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+			};
+			*/
+
+			$().UItoTop({
+				easingType: 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<!-- //smooth-scrolling-of-move-up -->
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="js/bootstrap.js"></script>
+</body>
+
+</html>
